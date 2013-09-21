55d88f29c2d445437d7e7b062fa98d1d182849bb
Florian <pulv3rtoastmann@gmail.com>
2013-09-21 14:57:03 +0200

    grob

---
+++ src/de/hsb/simon/client/net/ClientInterfaceImpl.java
@@ -0,0 +1,16 @@
+package de.hsb.simon.client.net;
+
+import de.hsb.simon.commons.ServerInterface;
+import de.hsb.simon.commons.SessionInterface;
+import de.root1.simon.Lookup;
+
+public class ClientInterfaceImpl {
+
+	private Lookup lookup;
+	private ServerInterface server;
+	private SessionInterface session;
+	
+	public ClientInterfaceImpl() {
+		//
+	}
+}
